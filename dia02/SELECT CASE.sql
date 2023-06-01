7fd7cba48984ef7b46e0d9f902ec1e742a2207fc
volponidaniel@gmail.com <volponidaniel@gmail.com>
2023-06-01 01:49:59 +0000

    Aula de Select Case

---
+++ dia02/SELECT CASE.sql
@@ -0,0 +1,49 @@
+-- Databricks notebook source
+-- DBTITLE 1,Criando Faixa de Preço
+SELECT 
+  *
+  ,CASE
+    WHEN vlPreco <= 100 THEN '000 -| 100'
+    WHEN vlPreco <= 200 THEN '100 -| 200'
+    WHEN vlPreco <= 300 THEN '200 -| 300'
+    WHEN vlPreco <= 400 THEN '300 -| 400'
+    WHEN vlPreco <= 500 THEN '400 -| 500'
+    WHEN vlPreco <= 600 THEN '500 -| 600'
+    WHEN vlPreco <= 700 THEN '600 -| 700'
+    WHEN vlPreco <= 800 THEN '700 -| 800'
+    WHEN vlPreco <= 900 THEN '800 -| 900'
+    WHEN vlPreco <= 1000 THEN '900 -| 1000'
+    ELSE '+1000'
+  end as fxPreco
+FROM silver.olist.item_pedido
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Níveis de Frete
+SELECT 
+  *
+  ,CASE
+    WHEN vlFrete / (vlFrete + vlPreco) = 0 THEN 'Frete Gratuito'
+    WHEN vlFrete / (vlFrete + vlPreco) <= 0.2 THEN 'Frete Baixo'
+    WHEN vlFrete / (vlFrete + vlPreco) <= 0.4 THEN 'Frete Médio'
+    ELSE 'Frete Alto'
+  end as descFrete
+FROM silver.olist.item_pedido
+
+-- COMMAND ----------
+
+SELECT 
+  *
+  ,CASE
+    WHEN descUF in ('SC', 'PR', 'RS') THEN 'Sul'
+    WHEN descUF in ('ES', 'RJ', 'SP', 'MG') THEN 'Sudeste'
+    WHEN descUF in ('MS', 'MT', 'DF', 'GO') THEN 'Centro-Oeste'
+    WHEN descUF in ('AC', 'RO', 'AM', 'RR', 'PA', 'AP', 'TO') THEN 'Norte'
+    ELSE 'Nordeste'
+  END as descRegiao
+FROM silver.olist.cliente
+
+-- COMMAND ----------
+
+SELECT *
+from silver.olist.vendedor
